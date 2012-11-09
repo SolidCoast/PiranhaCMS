7e18cb6fbbd7bca8df4c3c005f787fe39c38e10c
SolidCoast <sc@solidcoast.org>
2012-11-09 00:11:15 -0500

    Removed unused references

--- App_Code/WebApplication.cs
+++ App_Code/WebApplication.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-using Piranha.WebPages;
+﻿using Piranha.WebPages;
 
 /// <summary>
 /// Summary description for Class1
